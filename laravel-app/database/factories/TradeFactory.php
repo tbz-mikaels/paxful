b13317d20326a8ccdf832c02912e7072f6571e81
Talha <talha.zafar@mikaels.com>
2020-03-27 16:07:38 +0500

    Minor Changes..

--- laravel-app/database/factories/TradeFactory.php
+++ laravel-app/database/factories/TradeFactory.php
@@ -10,7 +10,7 @@ $factory->define(Trade::class, function (Faker $faker) {
     $statuses = Trade::ALLOWED_STATUS;
 
     return [
-        'buyer_username' => $faker->name,
+        'buyer_username' => $faker->userName,
         'payment_method' => $methods[array_rand($methods)],
         'amount' => $faker->biasedNumberBetween(10, 100),
         'status' => $statuses[array_rand($statuses)],
